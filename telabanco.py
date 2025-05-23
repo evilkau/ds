fb4d1e69741bd001b560b0bcb7cc61e4b8998abe
evilkau <146204994+evilkau@users.noreply.github.com>
2025-05-23 11:58:34 -0300

    Add files via upload

---
+++ telabanco.py
@@ -0,0 +1,11 @@
+from conta import Conta 
+
+conta1= Conta(123,"10500-10","kauany",7.0,1234)
+print(f'saldo em conta corrente:{conta1.extrato()}')
+
+if conta1.sacar(8)
+    print("saque efetuado com suceso!")
+else:
+    print("saque negado!")
+
+print(f'saldo em conta corrente:{conta1.extrato()}')   
